b26e4dd489877490beeebedfe5e32bbab99442e0
Joubin Jabbari <joubin@jabbari.io>
2021-09-10 14:45:48 -0700

    test

--- main.py
+++ main.py
@@ -43,7 +43,8 @@ def ffmpeg_stream(stream_url: str, segment_time: int = 300, output_path: str = "
     # Ensure that url is from a trusted source
     command = "ffmpeg -i " + stream_url + " -c:v libx264  -f segment -segment_time " + str(
         segment_time) + " -g 10 -sc_threshold 0 " \
-                        "-reset_timestamps 1 -strftime 1 "+OUTPUT_DIR+"%Y-%m-%d_%H-%M-%S-Garage.mp4 "
+                        "-reset_timestamps 1 -strftime 1 "+OUTPUT_DIR+"%Y-%m-%d_%H-%M-%S-Garage.mp4"
+    print(command)
     subprocess.call(command, shell=True)
 
 
